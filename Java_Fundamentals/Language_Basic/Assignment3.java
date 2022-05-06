efbee93ab25fac50458b572fa28f4c23c2117a38
Aditya Galande <adityagalande14@gmail.com>
2022-05-06 23:26:04 +0530

    Language basics

---
+++ Java_Fundamentals/Language_Basic/Assignment3.java
@@ -0,0 +1,7 @@
+public class Assignment3{
+
+     public static void main(String []args){
+         int res = Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+         System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+res);
+     }
+}
