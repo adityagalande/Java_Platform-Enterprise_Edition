84c239b97e50abe3d0e57b351541cb5f1acec929
Aditya Galande <adityagalande14@gmail.com>
2022-05-15 22:58:01 +0530

    Assignment1.java

---
+++ Wrapper_Classes/Wrapper_Classes/Assignment1.java
@@ -0,0 +1,19 @@
+public class Assignment1{
+
+     public static void main(String []args){
+         short shortMinValue = Short.MIN_VALUE;
+         short shortMaxValue = Short.MAX_VALUE;
+         byte byteMinValue = Byte.MIN_VALUE;
+         byte byteMaxValue = Byte.MAX_VALUE;
+         int intMinValue = Integer.MIN_VALUE;
+         int intMaxValue = Integer.MAX_VALUE;
+         long longMinValue = Long.MIN_VALUE;
+         long longMaxValue = Long.MAX_VALUE;
+         float floatMinValue = Float.MIN_VALUE;
+         float floatMaxValue = Float.MAX_VALUE;
+         double doubleMinValue = Double.MIN_VALUE;
+         double doubleMaxValue = Double.MAX_VALUE;
+         
+        System.out.println("shortMinValue : "+shortMinValue+"\nshortMaxValue : "+shortMaxValue+"\nbyteMinValue : "+byteMinValue+"\nbyteMaxValue : "+byteMaxValue+"\nintMinValue : "+intMinValue+"\nintMaxValue : "+intMaxValue+"\nlongMinValue : "+longMinValue+"\nlongMaxValue : "+longMaxValue+"\nfloatMinValue : "+floatMinValue+"\nfloatMaxValue : "+floatMaxValue+"\ndoubleMinValue : "+doubleMinValue+"\ndoubleMaxValue : "+doubleMaxValue);
+     }
+}
