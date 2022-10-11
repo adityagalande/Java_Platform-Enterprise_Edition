980642dd91f1dfd4bfea831109684897d9d26032
Aditya Galande <adityagalande14@gmail.com>
2022-10-11 22:21:37 +0530

    Parallel Array Sorting Assignment02.java

---
+++ Java_8_Features/Parallel Array Sorting/Assignment02.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Assignment02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the string: ");
+		String str = scan.next();
+		char[] array = str.toCharArray();
+		Arrays.parallelSort(array);
+		str = new String(array);
+		System.out.println(str);
+	}
+
+}
